5d7ca9b37895dad534c5dbe92534b22391a28363
Vito Farenga <vitofarenga@libero.it>
2022-06-23 16:55:40 +0200

    prova codice crazione contenitore grande

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const contenitore = document.getElementById('container');
+
+const contenitore = document.getElementById('grid-container');
+
+
+
+for(let i = 1; i == 1; i++){
+    const quadratoG = creaQuadrato();
+
+    contenitore.append(quadratoG);
+}
+
+function creaQuadrato(){
+    const quadrato = document.createElement('div');
+    quadrato.classList.add('container');
+    return quadrato;
+}
