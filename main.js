a5e35c62151cc2bef09b76c930622cfb58919917
Vito Farenga <vitofarenga@libero.it>
2022-06-24 16:18:31 +0200

    ancora qui

--- main.js
+++ main.js
@@ -5,18 +5,23 @@ const contenitore = document.getElementById('container');
 
 
 
-const button = document.getElementById('btn')
+for(let i = 0; i < 100; i++){
+    const quadratino = creaQuadrato();
 
-button.addEventListener("click"){
-    for(let i = 0; i < 100; i++){
-        const quadratino = creaQuadrato();
-    
-        contenitore.append(quadratino);
-    }
-    
-    function creaQuadrato(){
-        const quadrato = document.createElement('div');
-        quadrato.classList.add('blocco');
-        return quadrato;
-    }
+    contenitore.append(quadratino);
+
+    quadratino.addEventListener('click', function(){
+        quadratino.classList.toggle('green')
+    })
 }
+
+const button = document.getElementById('btn');
+function creaQuadrato(){
+    const quadrato = document.createElement('div');
+    quadrato.classList.add('blocco');
+    return quadrato;
+}
+
+button.addEventListener('click', function(){
+    creaQuadrato()
+})
